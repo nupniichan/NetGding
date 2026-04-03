7f67e4e3ae81d76ea3fc1ab170bc5d0e342912a6
nupniichan <bao96065@gmail.com>
2026-04-04 00:38:27 +0700

    feat(Models): add Trend indicators model

---
+++ Models/Indicators/Trends/EMA.cs
@@ -0,0 +1,9 @@
+﻿namespace NetGding.Models.Indicators.Trends
+{
+    // Exponential Moving Average
+    public class EMA
+    {
+        public static readonly List<int> Periods = new List<int> { 9, 21, 34, 50, 89, 100, 200 };
+        public Dictionary<string, float> Values { get; set; } = new Dictionary<string, float>();
+    }
+}
---
+++ Models/Indicators/Trends/MACD.cs
@@ -0,0 +1,10 @@
+﻿
+namespace NetGding.Models.Indicators.Trends
+{
+    // Moving Average Convergence Divergence
+    public class MACD
+    {
+        public static readonly List<int> Periods = new List<int> { 12, 26, 9};
+        public Dictionary<string, float> Values { get; set; } = new Dictionary<string, float>();
+    }
+}
